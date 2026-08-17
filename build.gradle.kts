8f77796ceb5799db67dddab173f63a642727f497
bielbertisouzak-ship-it <biel.berti.souzak@gmail.com>
2026-08-16 23:23:18 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    id("com.android.application")
+    id("com.android.application") version "8.7.3"
 }
+
 android {
     namespace = "com.walkieoffline"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.walkieoffline"
         minSdk = 23
@@ -11,13 +13,11 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
     sourceSets {
         getByName("main") {
             java.srcDirs(".")
             manifest.srcFile("AndroidManifest.xml")
         }
     }
-}
-dependencies {
-    implementation("com.google.android.gms:play-services-nearby:19.3.0")
 }
